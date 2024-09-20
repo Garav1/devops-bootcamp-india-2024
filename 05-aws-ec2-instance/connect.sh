1c05e137069f081efc05f11bc9616bce040495d9
Garav1 <garav.allen123@gmail.com>
2024-09-20 06:41:22 +0000

    changes

--- 05-aws-ec2-instance/connect.sh
+++ 05-aws-ec2-instance/connect.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-PEM_FILE_PATH="./.certs/aws-ec2-keypair.pem"
-INSTANCE_PUBLIC_IP_ADDRESS="ec2-34-227-108-6.compute-1.amazonaws.com"
+PEM_FILE_PATH="./.certs/aws-ec2-garav3.pem"
+INSTANCE_PUBLIC_IP_ADDRESS="ec2-100-24-117-105.compute-1.amazonaws.com"
 # "34.227.108.6"
 
 
